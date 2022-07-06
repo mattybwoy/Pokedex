90d3a85ebc791b3123828ccc20bb36ed4f1eeb19
Matthew Lock <matthew_lock20@hotmail.com>
2022-07-06 18:06:06 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Matthew Lock on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
