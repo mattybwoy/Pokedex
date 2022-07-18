06515d23558173f140dcddebe059e6483ae6b60e
Matthew Lock <matthew_lock20@hotmail.com>
2022-07-18 17:41:23 +0100

    uploading deleted APIType file

---
+++ Pokedex/Models/APIType.swift
@@ -0,0 +1,13 @@
+//
+//  APIType.swift
+//  Pokedex
+//
+//  Created by Matthew Lock on 18/07/2022.
+//
+
+import Foundation
+
+enum APIType: String {
+    case pokemonList = "https://pokeapi.co/api/v2/pokemon/?limit=151"
+    case pokemonDetail = "https://pokeapi.co/api/v2/pokemon/"
+}
