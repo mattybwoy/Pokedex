4615f3626ea2994a57da81f5b550bd64ca57cbb3
Matthew Lock <matthew_lock20@hotmail.com>
2022-07-23 17:13:57 +0100

    PrettyAxis installed as dependency

---
+++ Pokedex/Views/ChartView.swift
@@ -0,0 +1,22 @@
+//
+//  ChartView.swift
+//  Pokedex
+//
+//  Created by Matthew Lock on 23/07/2022.
+//
+
+import SwiftUI
+import PrettyAxis
+
+struct ChartView: View {
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
--- Pokedex/Views/DetailView.swift
+++ Pokedex/Views/DetailView.swift
@@ -16,39 +16,44 @@ struct DetailView: View {
             Color(.white)
                 .ignoresSafeArea()
             if let selected = vm.selectPokemon {
-            CardBorderView(pokemonColorType: selected.types[0].type.name)
-            VStack {
-                HStack {
-                    Image("\(selected.types[0].type.name)Type")
-                        .resizable()
-                        .offset(y: -10)
-                        .frame(width: 50, height: 50)
-                        .padding(.horizontal, 80)
-                    Spacer()
-                    Text("\(selected.stats[0].base_stat)HP")
-                        .baselineOffset(-5)
-                        .offset(y: -10)
-                        .font(.custom("PokemonGB", size: 15))
-                        .padding(.trailing, 80)
-                }
+                CardBorderView(pokemonColorType: selected.types[0].type.name)
+                VStack {
+                    HStack {
+                        Image("\(selected.types[0].type.name)Type")
+                            .resizable()
+                            .offset(y: -10)
+                            .frame(width: 50, height: 50)
+                            .padding(.horizontal, 80)
+                        Spacer()
+                        Text("\(selected.stats[0].base_stat)HP")
+                            .baselineOffset(-5)
+                            .offset(y: -10)
+                            .font(.custom("PokemonGB", size: 15))
+                            .padding(.trailing, 80)
+                    }
                     Text(selected.name)
                         .font(.custom("PokemonGB", size: 23))
                         .baselineOffset(-5)
                     PokemonBioView(vm: vm)
-                    .offset(y: -20)
+                        .offset(y: -20)
                     Text("No: \(selected.id)")
                         .font(.custom("PokemonGB", size: 18))
+                        .padding(.bottom, 5)
                     Text("Height: " + "\(vm.pokemonHeight) cm")
                         .font(.custom("PokemonGB", size: 12))
                     Text("Weight: " + "\(vm.pokemonWeight) kg")
                         .font(.custom("PokemonGB", size: 12))
                     Divider().frame(width: 250)
                         .background(.black)
-                DescriptionView(vm: vm)
+                    TabView {
+                        DescriptionView(vm: vm)
+                        Text("Bananas")
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                     .frame(width: 270, height: 130)
-                Divider().frame(width: 250)
-                    .background(.black)
-                PokemonTypeView(vm: vm)
+                    Divider().frame(width: 250)
+                        .background(.black)
+                    PokemonTypeView(vm: vm)
                 }
             }
         }
